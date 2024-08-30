ec959016fa49d760d9997f346e5818de620e8d42
Elif Karadeniz <81620156+elifkaradenizz@users.noreply.github.com>
2024-08-30 14:29:30 +0300

    input oluşturuldu.

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-import streamlit as st 
+import streamlit as st
+
+# Uygulama başlığı
+st.title("Metin Özetleme Uygulaması")
+
+# Kullanıcıdan metin girdisi alma
+user_input = st.text_area("Lütfen özetlemek istediğiniz metni buraya yapıştırın:")
+
+
