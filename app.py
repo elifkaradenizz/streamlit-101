c6ac75daefd223c7cc7309f13472afdaa82ec51d
Elif Karadeniz <81620156+elifkaradenizz@users.noreply.github.com>
2024-09-10 20:49:13 +0300

    metin öğeleri uygulandı.

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import streamlit as st
 
-# Uygulama başlığı
-st.title("Metin Özetleme Uygulaması")
+#metin öğeleri
+import streamlit as st
 
-# Kullanıcıdan metin girdisi alma
+st.title("Proje Başlığı")  # Başlık burada yazılacak
+st.header("Ana Başlık")  # Ana başlık burada yazılacak
+st.subheader("Alt Başlık")  # Alt başlık burada yazılacak
+st.text("Bu bölümde düz metin bilgileri paylaşabilirsiniz.")  # Düz metin burada yazılacak
+st.markdown("""
+### Markdown Başlığı
+Markdown formatında metin yazabilirsiniz. 
+- Madde 1
+- Madde 2
+
+**Kalın metin** ve *eğik metin* kullanabilirsiniz.
+""")  # Markdown içeriği burada yazılacak
+
+
+
+# Kullanıcıdan metin girdisi alma --> input 
 user_input = st.text_area("Lütfen özetlemek istediğiniz metni buraya yapıştırın:")
+
+
+
+
 
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import  warnings
 
 warnings.filterwarnings('ignore')
 
+#streamlit ile sayfa bilgilerini düzenleme --> st.set_page_config
 st.set_page_config(page_title="Customer Segmentation", page_icon=":bar_chart:", layout="wide")
 
 st.title(":bar_chart: Customer Segmentation")
